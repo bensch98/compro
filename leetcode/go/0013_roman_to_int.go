0a1e70af7897d8236ea1cc034b362b99dfed48f7
bensch98 <scheffler.benedikt@googlemail.com>
2023-02-21 18:50:50 +0100

    leetcode problem 13 in go

---
+++ leetcode/go/0013_roman_to_int.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+)
+
+func romanToInt(s string) int {
+	var v, lv, cv int
+	r2i := map[uint8]int{
+		'I': 1,
+		'V': 5,
+		'X': 10,
+		'L': 50,
+		'C': 100,
+		'D': 500,
+		'M': 1000,
+	}
+
+	for i := len(s) - 1; i >= 0; i-- {
+		cv = r2i[s[i]]
+		if cv < lv {
+			v -= cv
+		} else {
+			v += cv
+		}
+		lv = cv
+	}
+	return v
+}
+
+func main() {
+	s := "MCMXCIV"
+	i := romanToInt(s)
+	fmt.Println(i)
+}
